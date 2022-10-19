6e6d7d4dfb902cd087974da0f504f4088afaa3d1
Lucy Ruiz <lucyruiz846@gmail.com>
2022-10-18 19:47:38 -0700

    set environment for project. Startwith App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+    const[entries, setEntries] = useState([{status: 'first post'}])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Enter status</th>
+                    <th>Enter status</th>
+                    <th>Enter status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>staus1</td>
+                    <td>status2</td>
+                    <td>status3</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
   );
 }
